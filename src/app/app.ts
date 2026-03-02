3585fcb4226ad613a26a5406a71c418b0682589d
RafaAgui <aguilaralvear@yahoo.es>
2026-03-02 11:28:49 +0100

    feat: delete old components

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,6 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header'
 import { Footer } from './layout/footer/footer'
-import { NewsList } from "./news-list/news-list"
 
 
 @Component({
