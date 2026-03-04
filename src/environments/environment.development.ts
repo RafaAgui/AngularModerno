5e7bca1841655eb20e46122618679f1540046857
RafaAgui <aguilaralvear@yahoo.es>
2026-03-04 12:43:13 +0100

    feat: environtment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,10 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  apiUrl: 'https://api.unsplash.com',
+  accessKey: 'Zt9sXo2n8l3j1a7m9v5b0c6d4e8f2g1h3i4j5k6l7m8n9o0p1q2r3s4t5u6v7w8x9y0z',
+
+    // Additional company information
+  companyName: 'Rafael Aguilar',
+  companyEmail: 'rafael.aguilar@company.com',
+  companySlogan: 'Capturing moments, one click at a time.'
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,13 @@
-export const environment = {};
+export const environment = {
+  production: true,
+
+  //Api url and access key should be stored in environment variables for security reasons, but for the sake of this example, they are hardcoded here.
+  apiUrl: 'https://api.unsplash.com',
+  aplication: '886710',
+  accessKey: 'r5kcOFNBcqT50qLc-cY1LzncZRAbniycMyD2Ork_PzQ',
+  Secretkey: 'ubjIMnAPwity-yIeLwnCOZUS57bbVJLQJqmga_VPvLQ',
+  // Additional company information
+  companyName: 'Rafael Aguilar',
+  companyEmail: 'rafael.aguilar@company.com',
+  companySlogan: 'Capturing moments, one click at a time.'
+};
