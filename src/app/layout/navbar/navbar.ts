c29d1e045590925782d519044df48199ccd48334
RafaAgui <aguilaralvear@yahoo.es>
2026-03-18 12:47:06 +0100

    feat: theme dark and light

--- src/app/layout/navbar/navbar.ts
+++ src/app/layout/navbar/navbar.ts
@@ -2,6 +2,7 @@ import { Component, inject, computed } from '@angular/core';
 import { RouterLink, RouterLinkActive } from "@angular/router";
 import { MENU_OPTIONS } from '../services/menu-options';
 import { AuthService } from '../../auth/services/users.services'
+import { ThemeService } from '../services/theme.services'
 
 
 @Component({
@@ -12,10 +13,11 @@ import { AuthService } from '../../auth/services/users.services'
 })
 export class Navbar {
   menuOptions = MENU_OPTIONS;
-
+  themeService = inject(ThemeService);
   private authService = inject(AuthService);
 
   userName = computed(() => this.authService.user()?.name);
+  currentTheme = this.themeService.theme;
 
   logout() {
     this.authService.logout();
@@ -23,5 +25,9 @@ export class Navbar {
 
   login(u: string, p: string){
     this.authService.login(u, p);
+  }
+
+  toggle() {
+    this.themeService.toggleTheme();
   }
 }
